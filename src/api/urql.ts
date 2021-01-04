cf85cc2db2de8c96c1609a5b7a6d5b194fb351da
Saveg <savegdesigner@gmail.com>
2021-01-04 08:03:36 -0300

    cleaning client setup

--- src/api/urql.ts
+++ src/api/urql.ts
@@ -1,44 +1,5 @@
-import { createClient, dedupExchange, fetchExchange } from 'urql';
-
-import { cacheExchange } from '@urql/exchange-graphcache';
-
-import { recipesQuery } from '../graphql/queries/recipesQuery';
+import { createClient } from 'urql';
 
 export const urqlClient = createClient({
   url: 'http://localhost:4000/',
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      optimistic: {
-        createRecipe: (variables, _cache, _info) => {
-          //console.log(variables);
-          return {
-            __typename: 'Recipe',
-            name: variables.name,
-            description: variables.description,
-            ingredients: variables.ingredients,
-          };
-        },
-      },
-      updates: {
-        Mutation: {
-          createRecipe: (result, variables, _cache, _info) => {
-            _cache.updateQuery(
-              {
-                query: recipesQuery,
-              },
-              (cacheData) => {
-                console.log(result);
-                console.log(cacheData);
-                //cacheData?.recipes.unshift(result.createRecipe)
-
-                return cacheData;
-              }
-            );
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
 });
